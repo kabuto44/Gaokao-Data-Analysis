d51d49af58790ba2bc7b5173649998a2be432850
kabuto44 <90492587+kabuto44@users.noreply.github.com>
2024-04-14 23:10:00 -0400

    Import Yearbook data
    Import ~70 indicators from yearbook website, convert to cleaned data/metadata files, set a few constants for regional province sorting

--- consts.py
+++ consts.py
@@ -2,5 +2,14 @@ PROVINCES = ['Beijing', 'Tianjin', 'Hebei', 'Shanxi', 'Inner Mongolia', 'Liaonin
 PROV_DICT = {}
 for i in range(len(PROVINCES)):
     PROV_DICT[PROVINCES[i]]=i
+
+NORTH_CHINA = ['Beijing', 'Tianjin', 'Hebei', 'Shanxi', 'Inner Mongolia']
+NORTHEAST = ['Liaoning', 'Jilin', 'Heilongjiang']
+EAST_CHINA = ['Shanghai', 'Jiangsu', 'Zhejiang', 'Anhui', 'Fujian', 'Jiangxi', 'Shandong']
+CENTRAL_CHINA = ['Henan', 'Hubei', 'Hunan', 'Guangdong', 'Guangxi', 'Hainan']
+SOUTHWEST = ['Chongqing', 'Sichuan', 'Guizhou', 'Yunnan', 'Tibet']
+NORTHWEST = ['Shaanxi', 'Gansu', 'Qinghai', 'Ningxia', 'Xinjiang']
+NORTH_CHINA_MINUS_CITIES = ['Hebei', 'Shanxi', 'Inner Mongolia']
+EAST_CHINA_MINUS_CITIES = ['Jiangsu', 'Zhejiang', 'Anhui', 'Fujian', 'Jiangxi', 'Shandong']
 LASTYEAR = 2022
 FIRSTYEAR = 2004
