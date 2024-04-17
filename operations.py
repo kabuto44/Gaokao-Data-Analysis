818210478d03e76033a68e2849c5d1a4f594a6f8
kabuto44 <90492587+kabuto44@users.noreply.github.com>
2024-04-17 17:51:41 -0400

    Plot generation for attrition by cohort size and senior size

--- operations.py
+++ operations.py
@@ -6,10 +6,13 @@ import consts as const
 import yearbookops as ybk
 import ast
 
-def singleplot(variable, provinces = const.PROVINCES, xlabel = "Year", ylabel = None, legend_on = True):
+def singleplot(variable, provinces = const.PROVINCES, title = None,xlabel = "Year", ylabel = None, legend_on = True):
     plt.figure(figsize=(12,8))
     data,metadata = extract(variable,provinces)
-    plt.title(metadata["indicator"][0])
+    if title==None:
+        plt.title(metadata["indicator"][0])
+    else:
+        plt.title(title)
     plt.xlabel(xlabel)
     if ylabel == None:
         plt.ylabel(metadata["indicator"][0])
@@ -22,81 +25,23 @@ def singleplot(variable, provinces = const.PROVINCES, xlabel = "Year", ylabel = 
     plt.show()
 
 
-# def multiplot(indicators, provinces=const.PROVINCES,dates = None,title = None, ylabel = None,legend_on=True):
-#     plt.figure(figsize=(12,8))
-#     for variable in indicators:
-#         data, metadata = extract(variable,provinces)
-#         if dates == None:
-#             dates = metadata["dates"]
-#         for prov in metadata["regions"]:
-#             index = metadata["regions"][prov]
-#             prov_data = data[index]
-#             row_dates = dates.copy()
-#             new_data = []
-#             for yr in row_dates:
-#                 try:
-#                     i = metadata["dates"].index(yr)
-#                     datum = prov_data[i]
-#                     if datum == "nan":
-#                         row_dates.remove(yr)
-#                         continue
-#                     new_data.append(float(datum))
-#                 except IndexError:
-#                     row_dates.remove(yr)
-
-            
-#     for set in datasets:
-#         with open("Data/"+set+".csv","r") as csvfile:
-#             plots = csv.reader(csvfile)
-#             if provinces == None:
-#                 series = []
-#                 dates = []
-#                 labels = []
-#                 for row in plots:
-#                     if row[0]=="Region":
-#                         dates = row[2:]
-#                         continue
-#                     else:
-#                         name = row[0]
-#                         labels.append(name)
-#                         prov = []
-#                         for i in range(2,len(row)):
-#                             prov.append(float(row[i]))
-#                         series.append(prov)
-#                 dates.reverse()
-#                 for s in series:
-#                     s.reverse()
-#                 for i in range(0,len(series)):
-#                     plt.plot(dates,series[i],label=labels[i] + ", " + set)
-#             else:
-#                 dates = []
-#                 series = []
-#                 labels = []
-#                 for row in plots:
-#                     if row[0] == "Region":
-#                         dates = row[2:]
-#                         continue
-#                     elif row[0] in provinces:
-#                         prov = []
-#                         for i in range(2,len(row)):
-#                             prov.append(float(row[i]))
-#                         series.append(prov)
-#                         labels.append(row[0])
-#                         continue
-#                 dates.reverse()
-#                 for prov in series:
-#                     prov.reverse()
-#                 for i in range(0,len(series)):
-#                     plt.plot(dates,series[i],label=labels[i]+ ", " + set)
-#     if(title==None):
-#         plt.title(datasets)
-#     else:
-#         plt.title(title)
-#     plt.xlabel('Year', fontsize=10)
-#     plt.ylabel(ylabel, fontsize=10)
-#     if legend_on:
-#         plt.legend()
-#     plt.show()
+def saveplot(name,variable, provinces = const.PROVINCES, title = None, xlabel = "Year", ylabel = None, legend_on = True):
+    plt.figure(figsize=(12,8))
+    data,metadata = extract(variable,provinces)
+    if title==None:
+        plt.title(metadata["indicator"][0])
+    else:
+        plt.title(title)
+    plt.xlabel(xlabel)
+    if ylabel == None:
+        plt.ylabel(metadata["indicator"][0])
+    else:
+        plt.ylabel(ylabel)
+    for prov in metadata["regions"]:
+        plt.plot(metadata["dates"],data[metadata["regions"][prov]],label=prov)
+    if legend_on:
+        plt.legend()
+    plt.savefig("Plots/"+name+".pdf",bbox_inches="tight")
 
 
 def extract(path,select=const.PROVINCES):
